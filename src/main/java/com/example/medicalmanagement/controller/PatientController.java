251a4c7586a0f5132d3a010438ef6439c45e328d
RomeisaAliu <romeisa.aliu@albanialab.al>
2023-04-14 16:49:52 +0200

    get all doctors data stream

--- src/main/java/com/example/medicalmanagement/controller/PatientController.java
+++ src/main/java/com/example/medicalmanagement/controller/PatientController.java
@@ -1,13 +1,9 @@
 package com.example.medicalmanagement.controller;
 
-import com.example.medicalmanagement.dto.PatientDto;
 import com.example.medicalmanagement.service.PatientService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
-import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/patients")
--- src/main/java/com/example/medicalmanagement/service/PatientService.java
+++ src/main/java/com/example/medicalmanagement/service/PatientService.java
@@ -1,15 +1,10 @@
 package com.example.medicalmanagement.service;
 
-import com.example.medicalmanagement.dto.PatientDto;
-import com.example.medicalmanagement.model.Patient;
 import com.example.medicalmanagement.repository.PatientRepository;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Optional;
 
 @Service
 public class PatientService {
