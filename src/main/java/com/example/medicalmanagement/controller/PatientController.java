f0a85d0c967db587235226951956acbf284d4920
RomeisaAliu <romeisa.aliu@albanialab.al>
2023-04-20 10:40:20 +0200

    ok

--- src/main/java/com/example/medicalmanagement/controller/PatientController.java
+++ src/main/java/com/example/medicalmanagement/controller/PatientController.java
@@ -1,8 +1,11 @@
 package com.example.medicalmanagement.controller;
 
+import com.example.medicalmanagement.dto.PatientDto;
 import com.example.medicalmanagement.service.PatientService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 
 @RestController
--- src/main/java/com/example/medicalmanagement/service/PatientService.java
+++ src/main/java/com/example/medicalmanagement/service/PatientService.java
@@ -1,9 +1,14 @@
 package com.example.medicalmanagement.service;
 
+import com.example.medicalmanagement.dto.PatientDto;
+import com.example.medicalmanagement.model.Patient;
 import com.example.medicalmanagement.repository.PatientRepository;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 @Service
