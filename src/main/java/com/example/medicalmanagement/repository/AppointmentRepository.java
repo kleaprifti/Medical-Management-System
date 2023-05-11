b530c5f44ea9d313c0fe7cf22255f86df4fe148f
RomeisaAliu <romeisa.aliu@albanialab.al>
2023-05-11 17:21:32 +0200

    addAppointment update

--- src/main/java/com/example/medicalmanagement/repository/AppointmentRepository.java
+++ src/main/java/com/example/medicalmanagement/repository/AppointmentRepository.java
@@ -2,6 +2,8 @@ package com.example.medicalmanagement.repository;
 
 import com.example.medicalmanagement.model.Appointment;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.time.LocalDateTime;
@@ -13,6 +15,15 @@ public interface AppointmentRepository extends JpaRepository<Appointment,Long> {
 
     List<Appointment> findByDoctorIdAndAppointmentDateStartTimeBeforeAndAppointmentDateEndTimeAfter(Long  doctorId, LocalDateTime startDateTime, LocalDateTime endDateTime);
 
-    List<Appointment> findAppointmentsByDoctorId(Long doctorId);
+    @Query("SELECT a FROM Appointment a WHERE (a.patient.Id= :patientId OR a.doctor.Id = :doctorId) AND" +
+            "((a.appointmentDateStartTime < :endDateTime AND a.appointmentDateEndTime > :startDateTime) OR " +
+            "(a.appointmentDateStartTime >= :startDateTime AND a.appointmentDateStartTime < :endDateTime) OR " +
+            "(a.appointmentDateEndTime > :startDateTime AND a.appointmentDateEndTime <= :endDateTime))")
+    List<Appointment> findConflictingAppointments(@Param("patientId") Long patientId,
+                                                  @Param("doctorId") Long doctorId,
+                                                  @Param("startDateTime") LocalDateTime startDateTime,
+                                                  @Param("endDateTime") LocalDateTime endDateTime);
+
+
 
 }
--- src/main/java/com/example/medicalmanagement/service/AppointmentService.java
+++ src/main/java/com/example/medicalmanagement/service/AppointmentService.java
@@ -3,6 +3,7 @@ package com.example.medicalmanagement.service;
 import com.example.medicalmanagement.dto.AppointmentDto;
 import com.example.medicalmanagement.model.Appointment;
 import com.example.medicalmanagement.model.User;
+import com.example.medicalmanagement.model.UserRole;
 import com.example.medicalmanagement.repository.AppointmentRepository;
 import com.example.medicalmanagement.repository.UserRepository;
 import jakarta.persistence.EntityNotFoundException;
@@ -22,6 +23,7 @@ public class AppointmentService {
     private final AppointmentRepository appointmentRepository;
     private final UserRepository userRepository;
     private final ModelMapper modelMapper;
+
     @Autowired
     public AppointmentService(AppointmentRepository appointmentRepository, UserRepository userRepository, ModelMapper modelMapper) {
         this.appointmentRepository = appointmentRepository;
@@ -29,6 +31,7 @@ public class AppointmentService {
         this.modelMapper = modelMapper;
 
     }
+
     public Set<AppointmentDto> getAppointmentsBetweenDatesAndTimes(Long doctorId, LocalDateTime startDateTime, LocalDateTime endDateTime) {
         List<Appointment> currentAppointments = appointmentRepository.findByDoctorIdAndAppointmentDateStartTimeBeforeAndAppointmentDateEndTimeAfter(doctorId, endDateTime, startDateTime);
         return currentAppointments.stream()
@@ -36,53 +39,54 @@ public class AppointmentService {
                 .collect(Collectors.toSet());
     }
 
+
     public AppointmentDto addAppointment(AppointmentDto appointmentDto) {
-    try {
-        LocalDateTime now = LocalDateTime.now();
-        if (appointmentDto.getAppointmentDateStartTime().isBefore(now)) {
-            throw new IllegalArgumentException("Appointment time cannot be in the past");
-        }
-        List<Appointment> existingAppointments = appointmentRepository.findAppointmentsByDoctorId(appointmentDto.getDoctorId());
-
-        for (Appointment existingAppointment : existingAppointments) {
-            if (existingAppointment.getAppointmentDateStartTime().isBefore(appointmentDto.getAppointmentDateEndTime()) &&
-                    existingAppointment.getAppointmentDateEndTime().isAfter(appointmentDto.getAppointmentDateStartTime()) ||
-                    existingAppointment.getAppointmentDateStartTime() == (appointmentDto.getAppointmentDateEndTime()) &&
-                    existingAppointment.getAppointmentDateEndTime() == (appointmentDto.getAppointmentDateStartTime())) {
-
-                throw new IllegalArgumentException("An appointment already exists during this time range");
+        try {
+            LocalDateTime now = LocalDateTime.now();
+            if (appointmentDto.getAppointmentDateStartTime().isBefore(now)) {
+                throw new IllegalArgumentException("Appointment time cannot be in the past");
             }
+
+            List<Appointment> conflictingAppointment = appointmentRepository.findConflictingAppointments(appointmentDto.getPatientId(),appointmentDto.getDoctorId(),appointmentDto.getAppointmentDateStartTime(),appointmentDto.getAppointmentDateEndTime());
+            if (!conflictingAppointment.isEmpty()){
+                throw new IllegalArgumentException("This patient or this doctor has already an existing appointment during this time range");
+            }
+            LocalDateTime startDateTime = appointmentDto.getAppointmentDateStartTime();
+            LocalDateTime endDateTime = appointmentDto.getAppointmentDateEndTime();
+            if (startDateTime.plusHours(1).isAfter(endDateTime) || startDateTime.isEqual(endDateTime) || startDateTime.plusHours(1).isBefore(endDateTime)) {
+                throw new IllegalArgumentException("Appointment duration should be one hour");
+            }
+            User patient = userRepository.findById(appointmentDto.getPatientId())
+                    .orElseThrow(() -> new EntityNotFoundException("Patient not found"));
+            List<User> usersList = userRepository.findByRolesUserRole(UserRole.PATIENT);
+            if(!usersList.contains(patient)){
+                throw new IllegalArgumentException("Patient not found");
+            }
+            User doctor = userRepository.findById(appointmentDto.getDoctorId())
+                    .orElseThrow(() -> new EntityNotFoundException("Doctor not found"));
+            usersList = userRepository.findByRolesUserRole(UserRole.DOCTOR);
+            if(!usersList.contains(doctor)){
+                throw new IllegalArgumentException("Doctor not found");
+            }
+            Appointment appointment = new Appointment();
+            appointment.setAppointmentDateStartTime(startDateTime);
+            appointment.setAppointmentDateEndTime(endDateTime);
+            appointment.setPatient(patient);
+            appointment.setDoctor(doctor);
+            Appointment savedAppointment = appointmentRepository.save(appointment);
+
+            AppointmentDto savedAppointmentDto = new AppointmentDto();
+            savedAppointmentDto.setAppointmentId(savedAppointment.getAppointmentId());
+            savedAppointmentDto.setAppointmentDateStartTime(savedAppointment.getAppointmentDateStartTime());
+            savedAppointmentDto.setAppointmentDateEndTime(savedAppointment.getAppointmentDateEndTime());
+            savedAppointmentDto.setDoctorId(savedAppointment.getDoctor().getId());
+            savedAppointmentDto.setPatientId(savedAppointment.getPatient().getId());
+
+            return savedAppointmentDto;
+        } catch (IllegalArgumentException | EntityNotFoundException e) {
+            throw e;
+        } catch (Exception e) {
+            throw new RuntimeException("Technical issue occurred while adding appointment", e);
         }
-        LocalDateTime startDateTime = appointmentDto.getAppointmentDateStartTime();
-        LocalDateTime endDateTime = appointmentDto.getAppointmentDateEndTime();
-        if (startDateTime.plusHours(1).isAfter(endDateTime) || startDateTime.isEqual(endDateTime) || startDateTime.plusHours(1).isBefore(endDateTime)) {
-            throw new IllegalArgumentException("Appointment duration should be one hour");
-        }
-        User patient = userRepository.findById(appointmentDto.getPatientId())
-                .orElseThrow(() -> new EntityNotFoundException("Patient not found"));
-        User doctor = userRepository.findById(appointmentDto.getDoctorId())
-                .orElseThrow(() -> new EntityNotFoundException("Doctor not found"));
-
-        Appointment appointment = new Appointment();
-        appointment.setAppointmentDateStartTime(startDateTime);
-        appointment.setAppointmentDateEndTime(endDateTime);
-        appointment.setPatient(patient);
-        appointment.setDoctor(doctor);
-        Appointment savedAppointment = appointmentRepository.save(appointment);
-
-        AppointmentDto savedAppointmentDto = new AppointmentDto();
-        savedAppointmentDto.setAppointmentId(savedAppointment.getAppointmentId());
-        savedAppointmentDto.setAppointmentDateStartTime(savedAppointment.getAppointmentDateStartTime());
-        savedAppointmentDto.setAppointmentDateEndTime(savedAppointment.getAppointmentDateEndTime());
-        savedAppointmentDto.setDoctorId(savedAppointment.getDoctor().getId());
-        savedAppointmentDto.setPatientId(savedAppointment.getPatient().getId());
-
-        return savedAppointmentDto;
-    } catch (IllegalArgumentException | EntityNotFoundException e) {
-        throw e;
-    } catch (Exception e) {
-        throw new RuntimeException("Technical issue occurred while adding appointment", e);
     }
-  }
-
 }
