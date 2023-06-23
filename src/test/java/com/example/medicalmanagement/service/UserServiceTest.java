bde7b96cd15a0eec1b5443e848319c4fd27be28c
RomeisaAliu <romeisa.aliu@albanialab.al>
2023-06-23 11:51:20 +0200

    updating UserServiceTest - adding email to users

--- src/test/java/com/example/medicalmanagement/service/UserServiceTest.java
+++ src/test/java/com/example/medicalmanagement/service/UserServiceTest.java
@@ -36,20 +36,21 @@ import org.mockito.junit.jupiter.MockitoExtension;
 
     @BeforeEach
     public void setup() {
-        Speciality speciality1 = new Speciality(1L,"Speciality 1",null);
-        Speciality speciality2 = new Speciality(2L,"Speciality 2",null);
-
+        Speciality speciality1 = new Speciality(1L,"doctor1@gmail.com","Speciality 1",null);
+        Speciality speciality2 = new Speciality(2L,"doctor2@gmail.com","Speciality 2",null);
         Role role = new Role();
         role.setUserRole(UserRole.DOCTOR);
 
         User user1 = new User();
         user1.setId(1L);
+        user1.setEmail("doctor1@gmail.com");
         user1.setFullName("Doctor 1");
         user1.setRoles(Collections.singletonList(role));
         user1.setSpecialities(Collections.singletonList(speciality1));
 
         User user2 = new User();
         user2.setId(2L);
+        user2.setEmail("doctor2@gmail.com");
         user2.setFullName("Doctor 2");
         user2.setRoles(Collections.singletonList(role));
         user2.setSpecialities(Collections.singletonList(speciality2));
@@ -67,12 +68,14 @@ import org.mockito.junit.jupiter.MockitoExtension;
 
         UserDto doctorDto1 = doctorDtos.get(0);
         assertEquals(1L, doctorDto1.getId());
+        assertEquals("doctor1@gmail.com",doctorDto1.getEmail());
         assertEquals("Doctor 1", doctorDto1.getFullName());
         assertEquals(Collections.singletonList(UserRole.DOCTOR), doctorDto1.getRoles());
         assertEquals(Collections.singletonList("Speciality 1"), doctorDto1.getSpecialities());
 
         UserDto doctorDto2 = doctorDtos.get(1);
         assertEquals(2L, doctorDto2.getId());
+        assertEquals("doctor2@gmail.com",doctorDto2.getEmail());
         assertEquals("Doctor 2", doctorDto2.getFullName());
         assertEquals(Collections.singletonList(UserRole.DOCTOR), doctorDto2.getRoles());
         assertEquals(Collections.singletonList("Speciality 2"), doctorDto2.getSpecialities());
