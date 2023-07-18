2ba2d9caf7a9ad848171c17ca92547cebcc44065
sildiRicku <sildi.ricku@albanialab.al>
2023-07-18 18:08:22 +0200

    delete email

--- src/main/java/com/example/medicalmanagement/helpers/EmailContent.java
+++ src/main/java/com/example/medicalmanagement/helpers/EmailContent.java
@@ -9,7 +9,7 @@ public class EmailContent {
         String formattedAppointmentDate = appointmentDate.format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm"));
 
         return  "Hello " + userName + ",\n\n"
-                + "Your appointment  " +  " on " + formattedAppointmentDate + " is cancelled.";
+                + "Your appointment  " +  " on " + formattedAppointmentDate + " is cancelled due to a possible ferrari win"+ "\n\n"+"Regards\n The best online medical center";
 
     }
 }
