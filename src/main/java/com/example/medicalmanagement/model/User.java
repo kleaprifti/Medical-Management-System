1a8325ef33296770c40c02230084d12ea1745011
sildiRicku <sildi.ricku@albanialab.al>
2023-07-18 12:34:14 +0200

    delete email

--- src/main/java/com/example/medicalmanagement/model/User.java
+++ src/main/java/com/example/medicalmanagement/model/User.java
@@ -18,7 +18,7 @@ import java.util.List;
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long Id;
+    private Long id;
 
     @Column(name = "full_name")
     private String fullName;
