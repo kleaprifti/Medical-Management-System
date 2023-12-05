e92a1b13617b800590f34b031d5fcbf928577cec
RomeisaAliu <romeisa.aliu@albanialab.al>
2023-12-05 17:36:39 +0100

    update :)

--- src/main/java/com/example/medicalmanagement/controller/LoginController.java
+++ src/main/java/com/example/medicalmanagement/controller/LoginController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/login")
-@CrossOrigin
+@CrossOrigin(origins = "http://localhost:4200/")
 public class LoginController {
     @Autowired
     private LoginService loginService;
