61d911539df9f5e9d9a8bd1aa2ddf357ea01fe40
RomeisaAliu <romeisa.aliu@albanialab.al>
2024-01-08 12:53:27 +0100

    update security with remember me logic

--- src/main/java/com/example/medicalmanagement/config/SecurityConfig.java
+++ src/main/java/com/example/medicalmanagement/config/SecurityConfig.java
@@ -7,8 +7,12 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.RememberMeServices;
+import org.springframework.security.web.authentication.rememberme.TokenBasedRememberMeServices;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
 import static org.springframework.security.config.Customizer.withDefaults;
@@ -22,6 +26,7 @@ public class SecurityConfig  {
     @Autowired
     private CustomUserDetailsService customUserDetailsService;
 
+
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
@@ -32,17 +37,26 @@ public class SecurityConfig  {
                         .anyRequest().permitAll()
                 )
                 .httpBasic(withDefaults())
-                .rememberMe(rememberMe -> rememberMe.key("uniqueAndSecretKey").tokenValiditySeconds(86400))
+                .rememberMe(rememberMe -> rememberMe.key("uniqueAndSecretKey").tokenValiditySeconds(86400)
+                .userDetailsService(customUserDetailsService))
                 .csrf(csrf -> csrf.disable());
 
         return http.build();
     }
-
     @Autowired
     public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
         auth.userDetailsService(customUserDetailsService)
                 .passwordEncoder(passwordEncoder);
     }
-
-
+    @Autowired
+    RememberMeServices rememberMeServices(UserDetailsService userDetailsService) {
+        TokenBasedRememberMeServices.RememberMeTokenAlgorithm encodingAlgorithm = TokenBasedRememberMeServices.RememberMeTokenAlgorithm.SHA256;
+        TokenBasedRememberMeServices rememberMe = new TokenBasedRememberMeServices("uniqueAndSecretKey", userDetailsService, encodingAlgorithm);
+        rememberMe.setMatchingAlgorithm(TokenBasedRememberMeServices.RememberMeTokenAlgorithm.MD5);
+        return rememberMe;
+    }
+    @Autowired
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 }
