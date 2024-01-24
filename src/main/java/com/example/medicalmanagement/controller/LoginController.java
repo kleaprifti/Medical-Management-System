1c17b599aa38171768aaa5a0fc80a1add011d368
RomeisaAliu <romeisa.aliu@albanialab.al>
2024-01-24 17:36:53 +0100

    remember me jwt with login

--- src/main/java/com/example/medicalmanagement/controller/LoginController.java
+++ src/main/java/com/example/medicalmanagement/controller/LoginController.java
@@ -2,9 +2,14 @@ package com.example.medicalmanagement.controller;
 
 import com.example.medicalmanagement.dto.LoginInfoDto;
 import com.example.medicalmanagement.service.LoginService;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.RememberMeServices;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
@@ -14,23 +19,45 @@ import java.util.Map;
 @RequestMapping("/login")
 @CrossOrigin
 public class LoginController {
+
     @Autowired
     private LoginService loginService;
 
+    private final RememberMeServices rememberMeServices;
+
+    public LoginController(RememberMeServices rememberMeServices) {
+        this.rememberMeServices = rememberMeServices;
+    }
+
     @PostMapping("")
-    public ResponseEntity<Map<String, Object>> login(@RequestBody LoginInfoDto loginInfoDto) {
+    public ResponseEntity<Map<String, Object>> login(@RequestBody LoginInfoDto loginInfoDto,
+                                                     @RequestParam(value = "rememberMe", defaultValue = "false") boolean rememberMe, HttpServletRequest request,
+                                                     HttpServletResponse response) {
         boolean isAuthenticated = loginService.authenticateUser(loginInfoDto.getUsername(), loginInfoDto.getPassword());
 
-        Map<String, Object> response = new HashMap<>();
-        response.put("isAuthenticated", isAuthenticated);
+        Map<String, Object> responseBody = new HashMap<>();
+        responseBody.put("isAuthenticated", isAuthenticated);
 
         if (isAuthenticated) {
-            response.put("username", loginInfoDto.getUsername());
-            response.put("message", "Login successful");
-            return ResponseEntity.ok(response);
+            handleSuccessfulLogin(loginInfoDto, rememberMe, request,response, responseBody);
         } else {
-            response.put("message", "Invalid credentials");
-            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(response);
+            handleFailedLogin(responseBody);
         }
+
+        return ResponseEntity.status(isAuthenticated ? HttpStatus.OK : HttpStatus.UNAUTHORIZED).body(responseBody);
+    }
+
+    private void handleSuccessfulLogin(LoginInfoDto loginInfoDto, boolean rememberMe, HttpServletRequest request, HttpServletResponse response, Map<String, Object> responseBody) {
+        responseBody.put("username", loginInfoDto.getUsername());
+        responseBody.put("message", "Login successful");
+
+        if (rememberMe) {
+            Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+            rememberMeServices.loginSuccess(request, response, authentication);
+        }
+    }
+
+    private void handleFailedLogin(Map<String, Object> responseBody) {
+        responseBody.put("message", "Invalid credentials");
     }
 }
