7cb90a3a28a40b8c93fe009c03a77aef6875d1de
RomeisaAliu <romeisa.aliu@albanialab.al>
2024-04-09 16:00:51 +0200

    log out cors

--- src/main/java/com/example/medicalmanagement/controller/LogoutController.java
+++ src/main/java/com/example/medicalmanagement/controller/LogoutController.java
@@ -1,22 +1,34 @@
 package com.example.medicalmanagement.controller;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.support.SessionStatus;
 
-@Controller
+@RestController
+@RequestMapping("/logout")
+@CrossOrigin("localhost:4200/")
 public class LogoutController {
 
-    @PostMapping("/logout")
+    @PostMapping("")
     public ResponseEntity<String> logout(Authentication authentication) {
         if (authentication != null) {
             authentication.setAuthenticated(false);
         }
         return ResponseEntity.ok("Logged out successfully");
-    }
 
+    }
+//    @GetMapping
+//    public String logout(HttpServletRequest request, HttpServletResponse response) {
+//    HttpSession session = request.getSession(false);
+//    if (session != null) {
+//        session.invalidate();
+//    }
+//
+//    return "redirect:/";
+//}
 }
