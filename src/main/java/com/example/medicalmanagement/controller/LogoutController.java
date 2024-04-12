7bda34a940e60318c3897fa1c8e2301a737631ad
RomeisaAliu <romeisa.aliu@albanialab.al>
2024-04-12 16:58:33 +0200

    logout

--- src/main/java/com/example/medicalmanagement/controller/LogoutController.java
+++ src/main/java/com/example/medicalmanagement/controller/LogoutController.java
@@ -1,34 +1,31 @@
 package com.example.medicalmanagement.controller;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
-import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.Authentication;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.*;
-import org.springframework.web.bind.support.SessionStatus;
 
 @RestController
-@RequestMapping("/logout")
-@CrossOrigin("localhost:4200/")
+@RequestMapping("")
 public class LogoutController {
 
-    @GetMapping("")
-    public ResponseEntity<String> logout(Authentication authentication) {
+    @GetMapping("/logout")
+    public ResponseEntity<String> logout(HttpServletRequest request, HttpServletResponse response,
+                                         Authentication authentication) {
         if (authentication != null) {
             authentication.setAuthenticated(false);
         }
-        return ResponseEntity.ok("Logged out successfully");
 
+        HttpSession session = request.getSession(false);
+        if (session != null) {
+            session.invalidate();
+        }
+
+        return ResponseEntity.ok("Logged out successfully");
     }
-//    @GetMapping
-//    public String logout(HttpServletRequest request, HttpServletResponse response) {
-//    HttpSession session = request.getSession(false);
-//    if (session != null) {
-//        session.invalidate();
-//    }
-//
-//    return "redirect:/";
-//}
 }
